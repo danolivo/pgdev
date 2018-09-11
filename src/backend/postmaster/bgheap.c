8a0ec18f4ee384a6e34d3b1e6a69b5360c96b003
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2018-09-11 11:41:38 +0500

    1

--- src/backend/postmaster/bgheap.c
+++ src/backend/postmaster/bgheap.c
@@ -1725,7 +1725,7 @@ main_launcher_loop(void)
 //			timeout = timeout_change(timeout, -20);
 //		else
 		if (len > 0)
-			timeout = (timeout < 0) ? 1 : (timeout/10);
+			timeout = (timeout < 0) ? 1 : (timeout/2);
 
 		if (SHASH_Entries(wTab[heapcleaner_max_workers]) > (double)(wTab[heapcleaner_max_workers]->Header.ElementsMaxNum)/2.)
 			timeout = (timeout > 1) ? (timeout/1.5) : timeout;
