125c83adb18ad79caac40578018a8781a031f7dc
Andrei V. Lepikhov <lepihov@gmail.com>
2025-09-28 12:13:30 +0200

    Statistics on dirtied and allocated local buffers.
    It is a preliminary commit that tracks the state of temp buffers. The main goal
    of this statistic is to provide the optimiser with numbers to compute
    the cost estimation of flushing temporary buffers.
    
    Such a flush may be necessary if the optimiser decides to build a plan that
    includes a parallel section of the query, which involves scanning
    a temporary table.

--- src/backend/storage/buffer/localbuf.c
+++ src/backend/storage/buffer/localbuf.c
@@ -43,6 +43,10 @@ typedef struct
 
 int			NLocBuffer = 0;		/* until buffers are initialized */
 
+
+int allocated_localbufs = 0;
+int dirtied_localbufs = 0;
+
 BufferDesc *LocalBufferDescriptors = NULL;
 Block	   *LocalBufferBlockPointers = NULL;
 int32	   *LocalRefCount = NULL;
@@ -507,7 +511,10 @@ MarkLocalBufferDirty(Buffer buffer)
 	buf_state = pg_atomic_read_u32(&bufHdr->state);
 
 	if (!(buf_state & BM_DIRTY))
+	{
 		pgBufferUsage.local_blks_dirtied++;
+		dirtied_localbufs++;
+	}
 
 	buf_state |= BM_DIRTY;
 
@@ -568,6 +575,12 @@ TerminateLocalBufferIO(BufferDesc *bufHdr, bool clear_dirty, uint32 set_flag_bit
 	/* Clear earlier errors, if this IO failed, it'll be marked again */
 	buf_state &= ~BM_IO_ERROR;
 
+	if (buf_state & BM_DIRTY)
+	{
+		Assert(dirtied_localbufs > 0);
+		dirtied_localbufs--;
+	}
+
 	if (clear_dirty)
 		buf_state &= ~BM_DIRTY;
 
@@ -606,6 +619,12 @@ InvalidateLocalBuffer(BufferDesc *bufHdr, bool check_unreferenced)
 	int			bufid = -buffer - 1;
 	uint32		buf_state;
 	LocalBufferLookupEnt *hresult;
+
+	if (pg_atomic_read_u32(&bufHdr->state) & BM_DIRTY)
+	{
+		Assert(dirtied_localbufs > 0);
+		dirtied_localbufs--;
+	}
 
 	/*
 	 * It's possible that we started IO on this buffer before e.g. aborting
@@ -945,6 +964,7 @@ GetLocalBufferStorage(void)
 	this_buf = cur_block + next_buf_in_block * BLCKSZ;
 	next_buf_in_block++;
 	total_bufs_allocated++;
+	allocated_localbufs++;
 
 	/*
 	 * Caller's PinLocalBuffer() was too early for Valgrind updates, so do it
